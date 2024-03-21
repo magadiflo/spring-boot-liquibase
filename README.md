92ef85463cda4612400e9734ed95bfcfa9110967
Martín <magadiflo@gmail.com>
2024-03-21 17:06:40 -0500

    Inicio

---
+++ README.md
@@ -0,0 +1,6 @@
+# [Versionado de base de datos con Liquibase](https://www.linkedin.com/pulse/java-database-versioning-liquibase-tiago-melo/)
+
+- [Liquibase Documentation](https://docs.liquibase.com/home.html)
+
+---
+
---
+++ src/main/java/dev/magadiflo/springbootliquibase/MainApplication.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.springbootliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
